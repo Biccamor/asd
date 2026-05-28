0dba55d107c661bb6a4c47b81194deb7666b1bb6
Fabian <fabianmoraesb@gmail.com>
2026-05-28 09:52:59 +0200

    terminy

--- dynamiki/terminy.py
+++ dynamiki/terminy.py
@@ -1,11 +1,21 @@
 def terminy(T):
+    n = 0
+    for i in T:
+        n = max(i[0], n)
     
-    T.sort(key = lambda x: (x[0], -x[1]))
-    t = 0 
-    ans = 0
-    for d,g in T:
-        if d > t: 
-            ans += g 
-            t+=1
+    time = [-1]*(n)
+    ans = []
+    T.sort(key=lambda x: -x[1])
 
-    print(ans)
+    for d, g in T:
+        for j in range(d-1, -1, -1):
+            if time[j]==-1:
+                time[j] = g
+                ans.append((d,g))
+                break 
+    print(time)
+    return sum(time), ans
+
+T = [[2,100], [1,19], [2,27], [1,25], [3,15]]
+ans = terminy(T)
+print(ans)
